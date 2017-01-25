32203cddc9ac9514507aa108c540fc50e6fd60b7
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-01-25 22:06:08 +0300

    part 2 is added

--- CreateStruct/Program.cs
+++ CreateStruct/Program.cs
@@ -15,27 +15,32 @@ namespace CreateStruct
                 public string firstName;
                 public string lastName;
                 public int age;
+            public enum Genders: int{Male,Female};
+            public Genders gender;
 
-                public Person(string _firstName, string _lastName, int _age)
-                {
-                    firstName = _firstName;
-                    lastName = _lastName;
-                    age = _age;
-                }
-                public override string ToString()
-                {
-                    return firstName + " " + lastName + ", age " + age;
-                }
-
-
-
+            public Person(string _firstName, string _lastName, int _age, Genders _gender)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+                gender = _gender;
             }
 
-            static void Main(string[] args)
+            public override string ToString()
             {
-                Person p = new Person("Tony", "Allen", 32);
-                Console.WriteLine(p);
-                Console.ReadKey();
+                return firstName + " " + lastName + " (" + gender + "), age " + age;
+            }
+
+
+
+
+        }
+
+        static void Main(string[] args)
+            {
+            Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
 
             }
         }
