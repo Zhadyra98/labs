cf948e6e2747cb37cd7368ff01dd55c735a13ce6
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-01-26 12:46:30 +0300

    new solution is added

---
+++ LAB1_2student/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_2student
+{
+    class Program
+    {
+        class Student
+        {
+            public string firstName;
+            public string lastName;
+            public int gpa;
+
+            public Student(string _firstName, string _lastName, int _gpa)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                gpa = _gpa;
+
+            }
+
+            public override string ToString()
+            { 
+                return firstName + " " + lastName + " " + gpa;
+            }
+        }
+            static void Main(string[] args)
+        {
+            Student s = new Student("Alice","Kim",3);
+                Console.WriteLine(s.ToString());
+            Console.ReadKey();
+        }
+    }
+}
