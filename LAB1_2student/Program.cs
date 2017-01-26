124d0a7df973c37d461f4a6b27304a91164acd1e
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-01-26 12:51:12 +0300

    comments added

--- LAB1_2student/Program.cs
+++ LAB1_2student/Program.cs
@@ -9,12 +9,12 @@ namespace LAB1_2student
     class Program
     {
         class Student
-        {
+        {    //declaring new variable to the class Student
             public string firstName;
             public string lastName;
             public int gpa;
 
-            public Student(string _firstName, string _lastName, int _gpa)
+            public Student(string _firstName, string _lastName, int _gpa)//constructor for class Student 
             {
                 firstName = _firstName;
                 lastName = _lastName;
@@ -22,14 +22,14 @@ namespace LAB1_2student
 
             }
 
-            public override string ToString()
+            public override string ToString()//funcltion to return values
             { 
                 return firstName + " " + lastName + " " + gpa;
             }
         }
             static void Main(string[] args)
         {
-            Student s = new Student("Alice","Kim",3);
+            Student s = new Student("Alice","Kim",3);//initialisation of variables
                 Console.WriteLine(s.ToString());
             Console.ReadKey();
         }
