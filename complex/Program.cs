df0c50b2f60ab5300d37c98138e6b681511479b7
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-03-08 20:26:28 +0300

    New completed version of Complex with serialization

--- complex/Program.cs
+++ complex/Program.cs
@@ -4,19 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 namespace complex
 {
-
+[Serializable]
     public class Complex
     {
-        public int a;//to keep up value
-        public int b;//to keep down value
-
-        public Complex(int a, int b)
+        int a, b;
+        public Complex()
         {
-            this.a = a;
-            this.b = b;
+            a = 0;
+            b = 0;
+        }
+        public Complex(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+        public int A//used to have an access of each field of an objects Complex
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+
+        }
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+            }
         }
 
         public static int crt(int a1, int b1)
@@ -89,15 +118,27 @@ namespace complex
             c6.b /= d;
             return c6;
         }
-        /*public override string ToString()
+        public override string ToString()
         {
             return this.a + "/" + this.b;
-        }*/
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            XmlSerializer x2 = new XmlSerializer(typeof(Complex));//creating variavle for XML serialization
+            FileStream f2 = new FileStream("2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);//file to keep serializsd data
+
+            XmlSerializer x1 = new XmlSerializer(typeof(Complex));
+            FileStream f1 = new FileStream("1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            XmlSerializer x3 = new XmlSerializer(typeof(Complex));
+            FileStream f3 = new FileStream("3.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            XmlSerializer x4 = new XmlSerializer(typeof(Complex));
+            FileStream f4 = new FileStream("4.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
 
             Complex c1 = new Complex(4, 2);//first complex number to work with
             Complex c2 = new Complex(5, 6);
@@ -106,42 +147,37 @@ namespace complex
             Complex c5 = c1 * c2;
             Complex c6 = c1 / c2;
 
-            /*FileStream f1 = new FileStream("1.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            XmlSerializer x1 = new XmlSerializer(typeof(Complex));
-           
-            x1.Serialize(f1, c3);
-            f1.Close();*/
+            x2.Serialize(f2, c3);//process of serializing
+            f2.Close();//close it, to use file second time 
+            Complex c7 = new Complex();//creating new Complex variable to keep Deserialized Complex
+            FileStream fs2 = new FileStream("2.txt", FileMode.Open, FileAccess.Read);//file which keeps des.data
+            c7 = x2.Deserialize(fs2) as Complex;//process of deserialixing 
+            Console.WriteLine(c7);//writing data into console
+            fs2.Close();//close file 
 
-            XmlSerializer x2 = new XmlSerializer(typeof(Complex));
-            FileStream f2 = new FileStream("2.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            x2.Serialize(f2, c4);
-            f2.Close();
+            x1.Serialize(f1, c4);
+            f1.Close();
+            Complex c8 = new Complex();
+            FileStream fs1 = new FileStream("1.txt", FileMode.Open, FileAccess.Read);
+            c8 = x1.Deserialize(fs1) as Complex;
+            Console.WriteLine(c8);
+            fs1.Close();
 
-            XmlSerializer x3 = new XmlSerializer(typeof(Complex));
-            FileStream f3 = new FileStream("3.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             x3.Serialize(f3, c5);
             f3.Close();
+            Complex c9 = new Complex();
+            FileStream fs3 = new FileStream("3.txt", FileMode.Open, FileAccess.Read);
+            c9 = x3.Deserialize(fs3) as Complex;
+            Console.WriteLine(c9);
+            fs3.Close();
 
-            XmlSerializer x4 = new XmlSerializer(typeof(Complex));
-            FileStream f4 = new FileStream("4.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             x4.Serialize(f4, c6);
             f4.Close();
-
-
-            Complex c10 = new Complex(0,0);
-            FileStream fr = new FileStream("1.xml", FileMode.Open, FileAccess.Read);
-            c10 = x2.Deserialize(fr) as Complex;
-
+            Complex c10 = new Complex();
+            FileStream fs4 = new FileStream("4.txt", FileMode.Open, FileAccess.Read);
+            c10 = x4.Deserialize(fs4) as Complex;
             Console.WriteLine(c10);
-            fr.Close();
-
-
-            /*
-            Console.WriteLine(c3);
-            Console.WriteLine(c4);
-            Console.WriteLine(c5);
-            Console.WriteLine(c6);
-            */
+            fs4.Close();
 
             Console.ReadKey();
         }
